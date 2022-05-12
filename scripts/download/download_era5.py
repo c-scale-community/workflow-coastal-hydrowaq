5d311f47a2dff88ed30609df4655cf262e2acf43
backeb <backeb@gmail.com>
2022-05-12 10:53:20 +0200

    download correct dates for era5

--- scripts/download/download_era5.py
+++ scripts/download/download_era5.py
@@ -29,7 +29,7 @@ def download_era5(longitude_min, longitude_max, latitude_min, latitude_max, date
         i = datetime.strptime(date_min, '%Y-%m-%d').year
         yearstr.append(f'{i:0>2}')
     else:
-        for i in range(datetime.strptime(date_min, '%Y-%m-%d').year, datetime.strptime(date_max, '%Y-%m-%d').year, 1):
+        for i in range(datetime.strptime(date_min, '%Y-%m-%d').year, datetime.strptime(date_max, '%Y-%m-%d').year+1, 1):
             yearstr.append(f'{i}')
 
     monthstr = []
@@ -37,7 +37,7 @@ def download_era5(longitude_min, longitude_max, latitude_min, latitude_max, date
         i = datetime.strptime(date_min, '%Y-%m-%d').month
         monthstr.append(f'{i:0>2}')
     else:
-        for i in range(datetime.strptime(date_min, '%Y-%m-%d').month, datetime.strptime(date_max, '%Y-%m-%d').month, 1):
+        for i in range(datetime.strptime(date_min, '%Y-%m-%d').month, datetime.strptime(date_max, '%Y-%m-%d').month+, 1):
             monthstr.append(f'{i:0>2}')
 
     daystr = []
@@ -45,7 +45,7 @@ def download_era5(longitude_min, longitude_max, latitude_min, latitude_max, date
         i = datetime.strptime(date_min, '%Y-%m-%d').day
         daystr.append(f'{i:0>2}')
     else:
-        for i in range(datetime.strptime(date_min, '%Y-%m-%d').day, datetime.strptime(date_max, '%Y-%m-%d').day, 1):
+        for i in range(datetime.strptime(date_min, '%Y-%m-%d').day, datetime.strptime(date_max, '%Y-%m-%d').day+1, 1):
             daystr.append(f"{i:0>2}")
 
     c.retrieve(
