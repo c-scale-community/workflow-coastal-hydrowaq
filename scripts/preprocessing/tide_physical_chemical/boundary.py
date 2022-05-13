8ae8f93260f6d5f45004f40ee528d88a6e6a4ec7
lorincmeszaros <64486996+lorincmeszaros@users.noreply.github.com>
2022-05-13 17:04:26 +0200

    In boundary.py read "tref" from .mdu file

--- scripts/preprocessing/tide_physical_chemical/boundary.py
+++ scripts/preprocessing/tide_physical_chemical/boundary.py
@@ -1,17 +1,18 @@
 from coastserv.models.boundary import Boundary
-import datetime
+from datetime import datetime
 from pathlib import Path
 import click
+import os
 
 @click.command()
 @click.option('--interp', required=False, type=bool, default=False, help='True/False then regular grid interpolation is conducted on 48 local cells, otherwise nearest neighbour (default: False)')
 @click.option('--simultaneous', required=False, type=bool, default=False, help='True/False-- if true then all data is interpolated at the same time across entire domain(default: False)')
 @click.option('--steric', required=True, default=True, type=str, help='True/False -- if true then steric boundary will be created')
-@click.option('--tref', required=True, type=str, help='YYYY,MM,DD,hh,mm,ss (Reference time in DFlowFM model)')
+#@click.option('--tref', required=True, type=str, help='YYYY,MM,DD,hh,mm,ss (Reference time in DFlowFM model)')
 @click.option('--input', required=True, type=str, help='Folder downloaded data')
 @click.option('--model', required=True, type=str, help='Folder with FM model')
-@click.option('--output', required=True, type=str, help='Folder with output')
-def boundary(interp, simultaneous, steric, tref, input, output, model):
+@click.option('--output', required=True, type=str, help='Folder with output')     
+def boundary(interp, simultaneous, steric, input, output, model):
     "Read tide data from CMEMS and ERA download and convert to FM boundary conditions"
 
     model_path = Path(model)
@@ -19,8 +20,52 @@ def boundary(interp, simultaneous, steric, tref, input, output, model):
     fm_ext = list(model_path.glob('*.ext'))
     sub = ['Opal', 'PON1', 'POP1', 'POC1', 'OXY', 'NO3', 'PO4', 'salinity', 'temperature', 'uxuy', 'steric', 'Green', 'Diat', 'Si']
 
-    tref_list = [int(c) for c in tref.strip().split(',')]
-    tref = datetime.datetime(tref_list[0], tref_list[1], tref_list[2], tref_list[3], tref_list[4], tref_list[5])
+    #Get tref from model .mdu file
+    mdu_files=[]
+    for root, dirs, files in os.walk(model_path):
+        for file in files:
+            if file.endswith(".mdu"):
+                 mdu_files.append(os.path.join(root, file))
+    
+    #Specify text to be found in the .mdu file
+    text='Reference date'
+    
+    try:
+        with open(mdu_files[0],'r') as mdufile:
+            lines = mdufile.readlines()
+            new_list = []
+            idx = 0
+          
+            # looping through each line in the file
+            for line in lines:
+                  
+                # if line have the input string, get the index 
+                # of that line and put the
+                # line into newly created list 
+                if text in line:
+                    new_list.insert(idx, line)
+                    idx += 1
+          
+            # closing file after reading
+            mdufile.close()
+            
+            # if length of new list is 0 that means 
+            # the input string isn't
+            # found in the text file
+            if len(new_list)==0:
+                print("\n\"" +text+ "\" is not found in \"" +mdufile+ "\"!")
+            else:
+                tref_str=new_list[0].split()[2]
+                tref=datetime.strptime(tref_str, '%Y%m%d')
+                print(" ")
+                print("Model reference date is: {}".format(tref))
+                print(" ")
+                
+    # entering except block
+    # if input file doesn't exist 
+    except :
+      print("\nThe .mdu file doesn't exist!")
+    
 
     for ext in fm_ext:
         test = Boundary(ext, input, sub, tref, output)
@@ -34,4 +79,4 @@ if __name__ == '__main__':
 
 
 # docker run -it -v ~/git/use-case-hisea/scripts/processing/download2FM/:/app -v ~/data/hisea/download/:/data/input -v ~/git/use-case-hisea/fm_model/:/data/model -v ~/data/hisea/boundaries:/data/output --entrypoint /bin/bash hisea-processing
-# python boundary.py --interp true --simultaneous true --steric true --tref '2015,01,01,00,00,00' --input /data/input --output /data/output --model /data/model
+# python boundary.py --interp true --simultaneous true --steric true --input /data/input --output /data/output --model /data/model
