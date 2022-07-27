c1a9d12fedd5abd611e04eb214c431ec187704e2
Bjorn Backeberg <29396406+backeb@users.noreply.github.com>
2022-07-27 15:58:09 +0200

    Update download_cmems_biogeochemistry.py

--- scripts/download/download_cmems_biogeochemistry.py
+++ scripts/download/download_cmems_biogeochemistry.py
@@ -5,41 +5,50 @@ Creation Date: 2 May 2022
 Author: backeb <bjorn.backeberg@deltares.nl> Bjorn Backeberg
 """
 import click
+from datetime import timedelta, datetime
+from pathlib import Path
 import subprocess
-from datetime import datetime, timedelta
+import xarray as xr # note dependencies: dask, netCDF4
 
 @click.command()
-@click.option("--username", default="", help="To get a username and password register at: https://resources.marine.copernicus.eu/registration-form")
-@click.option("--password", default="", help="To get a username and password register at: https://resources.marine.copernicus.eu/registration-form")
-@click.option("--longitude_min", default=22.5, help="Set minimum longitude for region of interest")
-@click.option("--longitude_max", default=24.5, help="Set maximum longitude for region of interest")
-@click.option("--latitude_min", default=36.5, help="Set minimum latitude for region of interest")
-@click.option("--latitude_max", default=38.5, help="Set maximum latitude for region of interest")
-@click.option("--date_min", default=(datetime.now()-timedelta(days=5)).strftime('%Y-%m-%d'), help="Set start date for data download. Format: YYYY-MM-DD. Default is today minus 5 days.")
-@click.option("--date_max", default=(datetime.now()).strftime('%Y-%m-%d'), help="Set end date for data download. Format: YYYY-MM-DD. Default is today.")
+@click.option('--username', default='', help='To get a username and password register at: https://resources.marine.copernicus.eu/registration-form')
+@click.option('--password', default='', help='To get a username and password register at: https://resources.marine.copernicus.eu/registration-form')
+@click.option('--longitude_min', type=(float), help='Minimum longitude for region of interest', default=-180, show_default=True)
+@click.option('--longitude_max', type=(float), help='Maximum longitude for region of interest', default=180, show_default=True)
+@click.option('--latitude_min', type=(float), help='Minimum latitude for region of interest', default=-90, show_default=True)
+@click.option('--latitude_max', type=(float), help='Maximum latitude for region of interest', default=90, show_default=True)
+@click.option('--date_min', type=(str), help='Start date for data download. Format: YYYY-MM-DD', default=(datetime.now()).strftime('%Y-%m-%d'), show_default=True)
+@click.option('--date_max', type=(str), help='End date for data download. Format: YYYY-MM-DD', default=(datetime.now()).strftime('%Y-%m-%d'), show_default=True)
+@click.option('--vars', multiple=True, help='List of available vars: https://catalogue.marine.copernicus.eu/documents/PUM/CMEMS-GLO-PUM-001-028.pdf', default=('no3','o2','phyc','po4','si'), show_default=True)
 
-def runcommand(username, password, longitude_min, longitude_max, latitude_min, latitude_max, date_min, date_max):
-    subprocess.run(["python", "-m", "motuclient", 
-    "--motu", "https://nrt.cmems-du.eu/motu-web/Motu",
-    "--service-id", "GLOBAL_ANALYSIS_FORECAST_BIO_001_028-TDS",
-    "--product-id", "global-analysis-forecast-bio-001-028-daily",
-    "--longitude-min",str(longitude_min),
-    "--longitude-max", str(longitude_max),
-    "--latitude-min", str(latitude_min),
-    "--latitude-max", str(latitude_max),
-    "--date-min", date_min+" 12:00:00",
-    "--date-max", date_max+" 12:00:00",
-    "--depth-min", "0.493",
-    "--depth-max", "5727.918000000001",
-    "--variable", "no3",
-    "--variable", "o2",
-    "--variable", "phyc",
-    "--variable", "po4",
-    "--variable", "si",
-    "--out-dir", "/data",
-    "--out-name", "cmems_no3_o2_phyc_po4_si.nc",
-    "--user", username,
-    "--pwd", password])
+def runcommand(username, password, longitude_min, longitude_max, latitude_min, latitude_max, date_min, date_max, vars):
+    #make the /data/tmp directory if it does not exist
+    Path('/data/tmp').mkdir(parents=True, exist_ok=True)
+    delta = datetime.strptime(date_max, '%Y-%m-%d') - datetime.strptime(date_min, '%Y-%m-%d')
+    for var in vars:
+        for i in range(delta.days+1):
+            day = datetime.strptime(date_min, '%Y-%m-%d').date() + timedelta(days=i)
+            check_file = Path('/data/tmp/cmems_'+str(var)+'_'+str(day)+'.nc')
+            while not check_file.is_file():
+                subprocess.run(["python", "-m", "motuclient", 
+                "--motu", "https://nrt.cmems-du.eu/motu-web/Motu",
+                "--service-id", "GLOBAL_ANALYSIS_FORECAST_BIO_001_028-TDS",
+                "--product-id", "global-analysis-forecast-bio-001-028-daily",
+                "--longitude-min",str(longitude_min),
+                "--longitude-max", str(longitude_max),
+                "--latitude-min", str(latitude_min),
+                "--latitude-max", str(latitude_max),
+                "--date-min", str(day)+" 12:00:00",
+                "--date-max", str(day)+" 12:00:00",
+                "--depth-min", "0.493",
+                "--depth-max", "5727.918000000001",
+                "--variable", str(var),
+                "--out-dir", "/data/tmp",
+                '--out-name', 'cmems_'+str(var)+'_'+str(day)+'.nc',
+                "--user", username,
+                "--pwd", password])
+        ds = xr.open_mfdataset('/data/tmp/cmems_'+var+'_*.nc')
+        ds.to_netcdf('/data/cmems_'+var+'.nc')
 
 if __name__ == '__main__':
     runcommand()
