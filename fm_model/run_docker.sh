1ddcb1dc311536891e88478b72aae1556c04918c
Bjorn Backeberg <29396406+backeb@users.noreply.github.com>
2022-09-02 12:44:40 +0000

    adding model files

--- fm_model/run_docker.sh
+++ fm_model/run_docker.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
+# To start Dimr, execute this script
 
-ulimit -s unlimited
+# stop after an error occured:
+set -e
 
-/opt/delft3dfm_latest/lnx64/bin/run_dimr.sh -c 8 --dockerparallel --D3D_HOME /opt/delft3dfm_latest/lnx64
+# Set numbers of hosts and cores per host
+nNodes=1
+#nProc=8
+nProc=1
+
+# set DIMR version to be used inside DOCKER: 
+dimrdir=/opt/delft3dfm_latest
+export PROC_DEF_DIR=$dimrdir/lnx64/share/delft3d
+
+# DOCKER: no queue selection
+#
+
+nPart=$((nNodes * nProc))
+
+# DIMR input-file; must already exist!
+dimrFile=dimr_config.xml
+
+# Replace number of processes in DIMR file
+PROCESSSTR="$(seq -s " " 0 $((nPart-1)))"
+sed -i "s/\(<process.*>\)[^<>]*\(<\/process.*\)/\1$PROCESSSTR\2/" $dimrFile
+
+# Read MDU file from DIMR-file
+mduFile="$(sed -n 's/\r//; s/<inputFile>\(.*\).mdu<\/inputFile>/\1/p' $dimrFile)".mdu
+
+# jobName: $FOLDERNAME
+export jobName="${PWD##*/}"
+
+
+if [ "$nPart" == "1" ]; then
+    $dimrdir/lnx64/bin/run_dimr.sh -m $dimrFile
+else
+    $dimrdir/lnx64/bin/run_dflowfm.sh --partition:ndomains=$nPart:icgsolver=6 $mduFile
+    #$dimrdir/lnx64/bin/run_dimr.sh --dockerparallel -c $nProc -m $dimrFile
+    $dimrdir/lnx64/bin/run_dimr.sh --dockerparallel -c $nProc -m $dimrFile >screen.log 2>&1
+fi
+
