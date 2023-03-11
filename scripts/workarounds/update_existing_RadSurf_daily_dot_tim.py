07f5290a6ae896b70b03ff94992ca258ff3e0ed9
Bjorn Backeberg <29396406+backeb@users.noreply.github.com>
2023-03-11 18:36:22 +0100

    Update update_existing_RadSurf_daily_dot_tim.py

--- scripts/workarounds/update_existing_RadSurf_daily_dot_tim.py
+++ scripts/workarounds/update_existing_RadSurf_daily_dot_tim.py
@@ -1,3 +1,29 @@
+# Author: [backeb](https://github.com/backeb)
+# This code reads in a RadSurf_daily.tim file and extends the timeseries to a date specified by the user. 
+# The new data is then written to an output file specified by the user.
+#
+# To use the script, the user needs to provide the following options as command-line arguments:
+#
+# --tref: Reference time as YYYY-MM-DD
+# --filename_in: Path/filename of input file
+# --filename_out: Path/filename of output file
+# --date_max: Date_max as YYYY-MM-DD
+#
+# Once the user provides the required options, they can run the script and it will
+#
+# 1. Read the data from the input file specified in --filename_in option.
+# 2. Add a datetime column based on the first column and reference time specified in --tref option.
+# 3. Determine the number of days between the last datetime in the last row and date_max specified in --date_max option.
+# 4. Append empty rows to the data to cover the missing days.
+# 5. Fill in the missing values in column 0 using previous value and unique differences.
+# 6. Fill in the missing datetime values using column 0 and reference time.
+# 7. Compute the average of values in column 1 for rows with the same day and month as the last row.
+# 8. Write the transformed data to a new file specified in --filename_out option with columns [0,1], separated by tabs and with floating point numbers in column 1 rounded to 8 decimal places.
+# The user can also add the --help option to see the help message and the list of available options.
+# , 
+# Below is an example of the scripts usage:
+#   `python update_existing_RadSurf_daily_dot_tim.py --tref '2015-01-01' --filename_in ../../fm_model/input/RadSurf_daily.tim --filename_out output.txt --date_max '2023-03-11'`
+
 
 import pandas as pd
 import click
@@ -7,10 +33,10 @@ import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
 @click.command()
-@click.option('--tref', type=str, help='Reference time')
-@click.option('--filename_in')
-@click.option('--filename_out')
-@click.option('--date_max')
+@click.option('--tref', type=str, help='Reference time as YYYY-MM-DD')
+@click.option('--filename_in', help='path/filename of input file')
+@click.option('--filename_out', help='path/filename of output file')
+@click.option('--date_max', help='date_max as YYYY-MM-DD')
 def main(tref, filename_in, filename_out, date_max):
 
     # Define the reference time
